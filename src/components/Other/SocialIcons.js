82951e55c5bf5e2ed0ea297f1402a2272f6b639e
jguigue <j.guigue@my-groom-service.com>
2021-04-12 16:43:33 +0200

    New RSN links

--- src/components/Other/SocialIcons.js
+++ src/components/Other/SocialIcons.js
@@ -6,30 +6,17 @@ export default function SocialIcons({ className, colored }) {
       <li>
         <a
           style={{ color: colored && "#2463ac" }}
-          href="https://www.facebook.com/"
+          href="https://www.facebook.com/Le-Temps-Vert-105540794967778"
         >
           <i className="fab fa-facebook-f" />
         </a>
       </li>
       <li>
-        <a style={{ color: colored && "#00c4fc" }} href="https://twitter.com">
-          <i className="fab fa-twitter" />
-        </a>
-      </li>
-      <li>
         <a
           style={{ color: colored && "#dd34c1" }}
-          href="https://instagram.com/"
+          href="www.instagram.com/letempsvert/"
         >
           <i className="fab fa-instagram" />
-        </a>
-      </li>
-      <li>
-        <a
-          style={{ color: colored && "#ff081c" }}
-          href="https://www.youtube.com/"
-        >
-          <i className="fab fa-youtube" />
         </a>
       </li>
     </ul>
